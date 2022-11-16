86f846ce28fffb7952d61b6f1cdb3b893e60d419
dytq <z5bu8mkb@duck.com>
2022-11-17 00:20:16 +0100

    canvas to bitmap

--- scripts/board.gd
+++ scripts/board.gd
@@ -1,16 +1,67 @@
-extends Node2D
+extends Sprite2D
 
-var current_line : Line2D
-var is_actived : bool
+var bitmap : BitMap
+var image : ImageTexture
 
-func _unhandled_input(event):
+var cache : Array
+const MAX_ARRAY_CACHE : int = 100
+
+const BUTTON_PRESSED = true
+const BUTTON_RELEASED = false
+var button_status = BUTTON_RELEASED
+
+var pad : float = 0.0 	# padding to interpolate (accurate) 2 points receive from mouse 
+var accuracy = 100 		# number of points between 2 points
+var accuracy_pad = 0.01
+
+const SIZE_SCREEN = Vector2(1152,648)
+
+
+func _ready():
+	image = ImageTexture.new()
+	bitmap = BitMap.new()
+	bitmap.create(Vector2(SIZE_SCREEN.x,SIZE_SCREEN.y))
+	self.texture = image.create_from_image(bitmap.convert_to_image())
+	self.position.x = SIZE_SCREEN.x / 2
+	self.position.y = SIZE_SCREEN.y / 2
+
+func _input(event):
 	if event is InputEventMouseButton:
-		current_line = Line2D.new()
-		current_line.default_color = Color.BLACK
-		current_line.width = 5.0
-		add_child(current_line)
-		is_actived = !is_actived
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				button_status = BUTTON_PRESSED
+			if event.pressed == false:
+				button_status = BUTTON_RELEASED
+				update_display()
+				cache.clear()
 
-func _process(_delta):
-	if(is_actived):
-		current_line.add_point(get_global_mouse_position())
+func update_display():
+	for i in range(0, cache.size()):
+		if(cache[i].x > 0 and cache[i].y > 0 and cache[i].x < SIZE_SCREEN.x and cache[i].y < SIZE_SCREEN.y):
+			bitmap.set_bit(cache[i].x, cache[i].y, true)
+	self.texture = image.create_from_image(bitmap.convert_to_image())
+
+func _process(delta):
+	var size_cache = cache.size()
+	
+	if button_status:
+		var current_path_position = get_viewport().get_mouse_position()
+		cache.append(current_path_position)
+		
+		size_cache = cache.size()
+		
+		if(size_cache > 1):
+			var ancient_path_position = cache[size_cache - 2]
+			var distance = current_path_position.distance_to(ancient_path_position)
+			
+			for _i in range(0, accuracy * distance * delta):
+				pad += (accuracy_pad/(distance * delta))
+				cache.append(ancient_path_position.lerp(current_path_position,pad))
+			pad = 0
+			
+			update_display()
+
+	if(size_cache > MAX_ARRAY_CACHE):
+		var last_element = cache[size_cache -1]
+		cache.clear()
+		cache.append(last_element) 
